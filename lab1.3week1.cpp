5de667b9248f9bd950970ac651977d440da5a21f
huseyinesen-code <huseyin.esen@std.izmirekonomi.edu.tr>
2026-03-08 18:09:16 +0300

    Add files via upload

---
+++ lab1.3week1.cpp
@@ -0,0 +1,14 @@
+ #include <iostream>
+using namespace std;
+int main() {
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout<<"Hello "<<name<<endl;
+
+    int student;
+    cout<<"Enter your student number: ";
+    cin>>student;
+    cout<<"Your student number is: "<<student<<endl;
+    return 0;
+}
